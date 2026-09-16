bf5305c8df9a1a0efe6600555bcf33040748c120
dreamsolister26 <oumaekumiko123@gmail.com>
2026-09-16 21:50:28 +0700

    earth: Keep Evolving!
    Signed-off-by: dreamsolister26 <oumaekumiko123@gmail.com>

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -11,8 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common EvolutionX stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+TARGET_ENABLE_FP_OVERRIDE := false
+PERF_ANIM_OVERRIDE := true
+TARGET_ENABLE_BLUR := false
+WITH_GMS := false
+TARGET_INCLUDE_GRAMOPHONE := true
+TARGET_INCLUDE_BCR := false
 
 PRODUCT_NAME := lineage_earth
 PRODUCT_DEVICE := earth
