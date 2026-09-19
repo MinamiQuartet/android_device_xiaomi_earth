2591189c2a7bf4f8c3a71586dc27a86fdae4a94e
Yoshikawa Yuuko <oumaekumiko123@gmail.com>
2026-09-19 23:23:51 +0700

    earth: Build Full GMS

--- lineage_earth.mk
+++ lineage_earth.mk
@@ -17,7 +17,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_ENABLE_FP_OVERRIDE := false
 PERF_ANIM_OVERRIDE := true
 TARGET_ENABLE_BLUR := false
-WITH_GMS := false
+WITH_GMS := true
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_GRAMOPHONE := true
 TARGET_INCLUDE_BCR := false
 
