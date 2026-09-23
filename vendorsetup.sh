51abec92be4a80eda56678e746c59fd8d73bf32a
dreamsolister26 <oumaekumiko123@gmail.com>
2026-09-23 11:03:30 +0700

    earth: Implement vendorsetup.sh
    Signed-off-by: dreamsolister26 <oumaekumiko123@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,13 @@
+echo "Cloning Stuffs Needed For Earth"
+
+# Other earth stuffs
+git clone https://github.com/MinamiQuartet/vendor_xiaomi_earth.git -b lineage-24.0-ims vendor/xiaomi/earth
+git clone https://github.com/MinamiQuartet/android_kernel_xiaomi_earth.git -b lineage-24.0 kernel/xiaomi/earth --depth=1
+git clone https://github.com/MinamiQuartet/minami-sign.git -b infinity vendor/infinity-priv/keys
+
+# Hardware Repos
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-24.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
+
+echo "All Done!, Let's Start!"
